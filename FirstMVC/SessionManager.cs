9410c00c2b8590d87edd53afc2c07c5328a87c1c
Jorge Aguilar <jorge.bravo77@gmail.com>
2024-08-17 12:19:39 -0600

    Add login forms

---
+++ FirstMVC/SessionManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+
+namespace FirstMVC
+{
+    public static class SessionManager
+    {
+        private const string UserSessionKey = "UserSession";
+
+        public static void SetUserSession(string email)
+        {
+            HttpContext.Current.Session[UserSessionKey] = email;
+        }
+
+        public static string GetUserSession()
+        {
+            return HttpContext.Current.Session[UserSessionKey] as string;
+        }
+
+        public static void ClearUserSession()
+        {
+            HttpContext.Current.Session.Remove(UserSessionKey);
+        }
+    }
+}
